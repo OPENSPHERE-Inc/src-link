5ff178b7aa1c4fc9f68e366be9df85a07c04f276
Shinichi Hanayama <hanatyan@opensphere.co.jp>
2024-11-22 10:08:37 +0900

    - Improve build script.

--- build-windows-installer.ps1
+++ build-windows-installer.ps1
@@ -1,3 +1,7 @@
+Param (
+    [switch]$installer
+)
+
 $BuildSpec = Get-Content -Path ./buildspec.json -Raw | ConvertFrom-Json
 $ProductName = $BuildSpec.name
 $ProductVersion = $BuildSpec.version
@@ -12,4 +16,7 @@ $env:CLIENT_SECRET = "replaceYourClientSecret"
 cmake --fresh -S . -B build_x64 -Wdev -Wdeprecated -DCMAKE_SYSTEM_VERSION="10.0.18363.657" -G "Visual Studio 17 2022" -A x64
 cmake --build build_x64 --config RelWithDebInfo --target ALL_BUILD --
 cmake --install build_x64 --prefix release/Package --config RelWithDebInfo
-iscc build_x64/installer-Windows.generated.iss /O"release" /F"${OutputName}-Installer-signed"
+
+if ($installer) {
+    iscc build_x64/installer-Windows.generated.iss /O"release" /F"${OutputName}-Installer-signed"
+}
